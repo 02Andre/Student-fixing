b5f2b11507bad1687fe124fbd2144e3c1902b7b1
02Andre <02andre.smith@gulllakecs.org>
2020-01-22 11:12:46 -0500

    no comments

--- ControlFlow.py
+++ ControlFlow.py
@@ -1,16 +1,8 @@
 
-"""
-Programmer: Zach Anderson
-Date: 12.16.19
-Running total Part 2
-"""
-
 sum = 0
 number_of_numbers = int(input("How many numbers would you like to sum? "))
 
 print("")
-
-
 for i in range(number_of_numbers):
     next = int(input("Enter a number: "))
     sum = sum + next
@@ -18,12 +10,6 @@ for i in range(number_of_numbers):
 print ("")
 print ("Sum: " + str(sum))
 
-
-"""
-Programmer: Zach Anderson
-Date: 12.16.19
-Average test scores
-"""
 total = 0
 how_many_tests = int(input("How many test do you need to average? "))
 
@@ -31,13 +17,9 @@ print("")
 for i in range(how many tests):
     score = float(input("Enter a score: "))
     total = total + score
-
 average = total / how_many_tests
 
 print("")
-
 print("Average: " + str(round(average, 2)))
-
 print("")
-
 print("The data type of the variable average is..." + str(type(average)))
